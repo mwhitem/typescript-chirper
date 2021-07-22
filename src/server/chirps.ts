7799ed6f094de6f7ca1b09a416a6a50448c6f670
Madison White <mvwhite32@gmail.com>
2021-07-22 14:45:58 -0500

    commit

--- src/server/chirps.ts
+++ src/server/chirps.ts
@@ -2,9 +2,9 @@
 // const chirpStore = require('./chirpstore');
 
 
-import React from 'react';
+// import React from 'react';
 import express from 'express'; 
-import chirpStore from './chirpstore';
+// import chirpStore from './chirpstore';
 import { GetChirps, GetChirp, UpdateChirp, CreateChirp, DeleteChirp } from "./chirpstore";
 
 
@@ -14,34 +14,34 @@ let Chirps = () => {
 
 
 router.get('/:id?', (req, res) => {
-    // let id = req.params.id
-    // if (id) {
-    //     res.json(chirpStore.GetChirp(id));
-    // } else {
+    let id = req.params.id
+    if (id) {
+        res.json(GetChirp(id));
+    } else {
 
-    //     res.send(chirpStore.GetChirps());
-    // }
-    const chirps = chirpStore.GetChirps()
+        res.send(GetChirps());
+    }
+    const chirps = GetChirps()
 let chirpArr: any[] = []
 Object.keys(chirps).map(key => chirpArr.push({id: key, name: chirps[key].name, msg: chirps[key].msg}))
 chirpArr.pop()
 });
 
 router.post('/', (req, res) => {
-    chirpStore.CreateChirp(req.body);
+    CreateChirp(req.body);
     res.sendStatus(200);
 });
 
 router.put('/:id', (req, res) => {
     const id = req.params.id
     const chirp = req.body
-    res.send(chirpStore.UpdateChirp(id, chirp))
+    res.send(UpdateChirp(id, chirp))
     res.sendStatus(200);
 });
 
 router.delete('/:id', (req, res) => {
     const id = req.params.id
-    res.send(chirpStore.DeleteChirp(id))
+    res.send(DeleteChirp(id))
 });
 }
 
