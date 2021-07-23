46760c955a86689534f61abcaf8cf12c88759ea0
Madison White <mvwhite32@gmail.com>
2021-07-23 09:46:59 -0500

    commit

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,46 +1,111 @@
+// import * as React from 'react';
+// import { useState, useEffect } from 'react';
+// import Chirps from '../server/routes/chirps'
+// /* HOOK REACT EXAMPLE */
+// const App = (props: AppProps) => {
+// 	// const [greeting, setGreeting] = useState<string>('');
+// 	// useEffect(() => {
+// 	// 	async function getGreeting() {
+// 	// 		try {
+// 	// 			const res = await fetch('/api/hello');
+// 	// 			const greeting = await res.json();
+// 	// 			setGreeting(greeting);
+// 	// 		} catch (error) {
+// 	// 			console.log(error);
+// 	// 		}
+// 	// 	}
+// 	// 	getGreeting();
+// 	// }, []);
+
+// 	return (
+// 		<>
+		
+// 		<nav className="navbar navbar-expand-lg navbar-light bg-info">
+//   <a className="navbar-brand" href="#">Navbar</a>
+//   </nav>
+// 		<main className="container my-5">
+// 			<h1 className="text-primary text-center">Send Your Chirp</h1>
+// 			<input type="text" name="name" placeholder="username"/>
+//             <input type="text" name="message" placeholder="your chirp"/>
+//             <input className="bg-dark text-white rounded" type="submit" value="send chirp"/>
+// 			<ul className="list-group">
+//   <li className="list-group-item">@ashsddhsk: hi</li>
+//   <li className="list-group-item">@rbfisnofnsk: hey</li>
+//   </ul>
+// 		</main>
+// 		{/* <Chirps/> */}
+// 		</>
+// 	);
+// };
+
+// interface AppProps {
+	
+// }
+
+/* CLASS REACT EXAMPLE */
+// class App extends React.Component<IAppProps, IAppState> {
+// 	constructor(props: IAppProps) {
+// 		super(props);
+// 		this.state = {
+// 			name: null
+// 		};
+// 	}
+
+// 	async componentDidMount() {
+// 		try {
+// 			let r = await fetch('/api/hello');
+// 			let name = await r.json();
+// 			this.setState({ name });
+// 		} catch (error) {
+// 			console.log(error);
+// 		}
+// 	}
+
+// 	render() {
+// 		return (
+// 			<main className="container my-5">
+// 				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
+// 			</main>
+// 		);
+// 	}
+// }
+
+// export interface IAppProps {}
+
+// export interface IAppState {
+// 	name: string;
+// }
+
+// export default App;
+
 import * as React from 'react';
 import { useState, useEffect } from 'react';
-import Chirps from '../server/chirps'
+
 /* HOOK REACT EXAMPLE */
 const App = (props: AppProps) => {
-	// const [greeting, setGreeting] = useState<string>('');
-	// useEffect(() => {
-	// 	async function getGreeting() {
-	// 		try {
-	// 			const res = await fetch('/api/hello');
-	// 			const greeting = await res.json();
-	// 			setGreeting(greeting);
-	// 		} catch (error) {
-	// 			console.log(error);
-	// 		}
-	// 	}
-	// 	getGreeting();
-	// }, []);
+	const [greeting, setGreeting] = useState<string>('');
+
+	useEffect(() => {
+		async function getGreeting() {
+			try {
+				const res = await fetch('/api/hello');
+				const greeting = await res.json();
+				setGreeting(greeting);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		getGreeting();
+	}, []);
 
 	return (
-		<>
-		
-		<nav className="navbar navbar-expand-lg navbar-light bg-info">
-  <a className="navbar-brand" href="#">Navbar</a>
-  </nav>
 		<main className="container my-5">
-			<h1 className="text-primary text-center">Send Your Chirp</h1>
-			<input type="text" name="username" placeholder="username"/>
-            <input type="text" name="message" placeholder="your chirp"/>
-            <input className="bg-dark text-white rounded" type="submit" value="send chirp"/>
-			<ul className="list-group">
-  <li className="list-group-item">@ashsddhsk: hi</li>
-  <li className="list-group-item">@rbfisnofnsk: hey</li>
-  </ul>
+			<h1 className="text-primary text-center">Hello {greeting}!</h1>
 		</main>
-		{/* <Chirps/> */}
-		</>
 	);
 };
 
-interface AppProps {
-	
-}
+interface AppProps {}
 
 /* CLASS REACT EXAMPLE */
 // class App extends React.Component<IAppProps, IAppState> {
---
+++ src/server/routes/chirps.ts
@@ -0,0 +1,111 @@
+// // const express = require('express');
+// // const chirpStore = require('./chirpstore');
+
+
+// // import React from 'react';
+// import express from 'express'; 
+// // import chirpStore from './chirpstore';
+// import { GetChirps, GetChirp, UpdateChirp, CreateChirp, DeleteChirp } from "./chirpstore";
+
+
+// let router = express.Router();
+
+// let Chirps = () => {
+
+
+// router.get('/:id?', (req, res) => {
+//     let id = req.params.id
+//     if (id) {
+//         res.json(GetChirp(id));
+//     } else {
+
+//         // res.send(GetChirps());
+//         const chirps = GetChirps()
+//     let chirpArr: any[] = []
+//     Object.keys(chirps).map(key => chirpArr.push({id: key, name: chirps[key].name, msg: chirps[key].msg}))
+//     chirpArr.pop()
+//     }
+// });
+
+// router.post('/', (req, res) => {
+//     CreateChirp(req.body);
+//     res.sendStatus(200);
+// });
+
+// router.put('/:id', (req, res) => {
+//     const id = req.params.id
+//     const chirp = req.body
+//     res.send(UpdateChirp(id, chirp))
+//     res.sendStatus(200);
+// });
+
+// router.delete('/:id', (req, res) => {
+//     const id = req.params.id
+//     res.send(DeleteChirp(id))
+// });
+// }
+
+// module.exports = router;
+
+// export default Chirps
+import * as express from "express";
+import { GetChirp, GetChirps, UpdateChirp, DeleteChirp, CreateChirp } from "../chirpstore";
+
+const router = express.Router();
+
+router.get('/:id?', (req, res) => { // /api/chirps/3
+  const id: string = req.params.id;
+
+  if (id) {
+    res.json(GetChirp(id));
+  } else {
+    const chirps = GetChirps();
+    let chirpArr: chirp[] = []
+
+    Object.keys(chirps).map(key => chirpArr.push(
+      {
+        id: key,
+        name: chirps[key].name,
+        message: chirps[key].message
+      }
+    ));
+
+    chirpArr.pop(); // eliminate nextid property 
+
+    res.json(chirpArr);
+  }
+});
+
+router.post('/', (req, res) => {
+  const chirpObj: chirp = req.body;
+
+  CreateChirp(chirpObj);
+
+  res.send("success");
+});
+
+//mandatory id param to tell the server which chirp to update
+router.put('/:id', (req, res) => {
+  const id: string = req.params.id;
+  const chirpObj: chirp = req.body;
+
+  UpdateChirp(id, chirpObj);
+
+  res.send("edited successfully");
+});
+
+router.delete('/:id', (req, res) => {
+  const id: string = req.params.id;
+
+  DeleteChirp(id);
+
+  res.send("deleted successfully");
+});
+
+interface chirp {
+  id?: string,
+  name: string,
+  message: string
+}
+
+export default router
